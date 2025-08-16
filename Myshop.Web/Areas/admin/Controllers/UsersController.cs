276bb0f5d07db097b89dc66439412d30c44cdaee
MarwanHussien1 <marwan11990077@gmail.com>
2025-08-17 00:58:31 +0300

    update Users controller

--- Myshop.Web/Areas/admin/Controllers/UsersController.cs
+++ Myshop.Web/Areas/admin/Controllers/UsersController.cs
@@ -1,30 +1,28 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Myshop.DataAccess.data;
-using Myshop.Entities.Repositories;
 using System.Security.Claims;
 using Utilities;
 
 
-namespace Myshop.Web.Areas.admin.Controllers
-{
-    [Area("Admin")]
+namespace Myshop.Web.Areas.admin.Controllers;
+
+	[Area("Admin")]
 	[Authorize(Roles = SD.AdminRole)]
 
 	public class UsersController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    public UsersController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
-        public UsersController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+        _context = context;
+    }
 
-        public IActionResult Index()
-        {
-            var claimsIdentity = (ClaimsIdentity)User.Identity;
-            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-            string userid = claim.Value;
-            return View(_context.ApplicationUsers.Where(x => x.Id != userid).ToList());
-        }
+    public IActionResult Index()
+    {
+        var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+        string userid = claim.Value;
+        return View(_context.ApplicationUsers.Where(x => x.Id != userid).ToList());
     }
 }
